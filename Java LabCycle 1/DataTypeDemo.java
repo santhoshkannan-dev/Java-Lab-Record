a7bf89804fd36715c48646749b2e89e5e3acb44d
santhoshkannan007 <myecommercesyt@gmail.com>
2026-06-27 10:15:33 +0530

    Add DataTypeDemo  program to LabCycle 1

---
+++ Java LabCycle 1/DataTypeDemo.java
@@ -0,0 +1,19 @@
+
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int i = 100;
+        long l = 100000;
+        float f = 25.5f;
+        double d = 123.456;
+        char ch = 'A';
+        boolean b = true;
+
+        System.out.println("Integer: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Character: " + ch);
+        System.out.println("Boolean: " + b);
+        
+    }
+}
