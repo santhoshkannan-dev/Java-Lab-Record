c5917fbfb85d5ddc559d29f3a1130f8fd8b92ffc
santhoshkannan007 <myecommercesyt@gmail.com>
2026-07-04 18:11:02 +0530

    Task 17  & 10 == 04/07/2026

---
+++ Java LabCycle 1/OperatorPrecedence.java
@@ -0,0 +1,7 @@
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+        int a=10, b=5, c=2, d=8, e=4;
+        int result = a + b * c - d / e;
+        System.out.println("Result = " + result);
+    }
+}
