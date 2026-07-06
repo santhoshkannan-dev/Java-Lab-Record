688696ddfc23174bf392807a8896091dcd4131d9
santhoshkannan007 <myecommercesyt@gmail.com>
2026-07-06 20:11:48 +0530

    Added LabCycle 2 folders and programs

---
+++ Java LabCycle 2/StudentDemo.java
@@ -0,0 +1,11 @@
+class Student {
+    Student() {
+        System.out.println("Welcome to Student Class");
+    }
+}
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student s = new Student(); // calls default constructor
+    }
+}
