0db38dd3ff3af0c52009aff58416dc1e9454dc55
santhoshkannan007 <myecommercesyt@gmail.com>
2026-07-24 13:10:07 +0530

    Test.java====== 2026/07/24

---
+++ Java Class Activites/Test.java
@@ -0,0 +1,18 @@
+class Animal {
+    void eat () {
+        System.out.println("Animal is Eating......");
+    }
+}
+class Dog extends Animal {
+    void eat () {
+        System.out.println("Dog is Eating......");
+        super.eat();
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+    }
+}
