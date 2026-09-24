2e82a6f8442a1e0bdac40f3495398f15ef0f63ae
santhoshkannan007 <myecommercesyt@gmail.com>
2026-09-24 17:48:00 +0530

    File Class program example

---
+++ Java Module-4/FileClass.java
@@ -0,0 +1,26 @@
+// 5. File class
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileClass {
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+            System.out.println("Absolute path: " + file.getAbsolutePath());
+            System.out.println("Writable: " + file.canWrite());
+            System.out.println("Readable: " + file.canRead());
+            System.out.println("Size: " + file.length() + " bytes");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
