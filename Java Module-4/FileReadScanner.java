40b36d6ed3696d5e507f4bb5c0aac108bf53e66c
santhoshkannan007 <myecommercesyt@gmail.com>
2026-09-24 17:49:55 +0530

    File Read using  Scanner program example

---
+++ Java Module-4/FileReadScanner.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReadScanner {
+    public static void main(String[] args) {
+        File file = new File("input.txt");
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
+}
